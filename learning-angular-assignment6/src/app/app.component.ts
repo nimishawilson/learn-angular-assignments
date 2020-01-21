cbd8dadd339a3ff2e1a8ca399f077d0740adc32a
Nimisha Wilson <nimishawilson123@gmail.com>
2020-01-21 12:19:22 +0530

    form with validation

--- learning-angular-assignment6/src/app/app.component.ts
+++ learning-angular-assignment6/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: false}) signupForm: NgForm;
+  user = {
+    email: '',
+    password: '',
+    subscriptions: ''
+  };
+  defaultSubscription = 'Advanced';
+
+
+  onSubmit(){
+  //  this.submitted = true;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.password = this.signupForm.value.userData.password;
+    this.user.subscriptions = this.signupForm.value.subscriptions;
+    this.signupForm.reset();
+    console.log('email: '+this.user.email)
+    console.log('password: '+this.user.password)
+    console.log('subscriptions: '+this.user.subscriptions)
+    console.log('-----------------')
+  }
 
 }
