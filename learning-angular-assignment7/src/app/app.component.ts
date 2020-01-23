e513e6713ad81ff7608447247c1ef8ae95e8df64
Nimisha Wilson <nimishawilson123@gmail.com>
2020-01-23 15:17:38 +0530

    add validators for form controls

--- learning-angular-assignment7/src/app/app.component.ts
+++ learning-angular-assignment7/src/app/app.component.ts
@@ -9,18 +9,32 @@ import {  FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class AppComponent {
   signupForm: FormGroup;
-
+  public projectName: string;
+   public email: any;
+    public projectStatus: string;
+  forbiddenProjectNames = ['test'];
+ 
    ngOnInit(){
     this.signupForm = new FormGroup({
-      'projectName' : new FormControl(null),
-      'email' : new FormControl(null),
+      'projectName' : new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+      'email' : new FormControl(null, [Validators.required, Validators.email]),
       'projectStatus': new FormControl(null)
     });
 }
 
+  forbiddenNames(control: FormControl): { [s:string]: boolean } {
+    if(this.forbiddenProjectNames.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
 
 onSubmit(){
-    console.log(this.signupForm)
+   console.log('--------')
+    console.log(this.signupForm.value.projectName)
+   this.projectName = this.signupForm.value.projectName;
+   this.email =  this.signupForm.value.email;
+   this.projectStatus = this.signupForm.value.projectStatus;
   }
 
 
